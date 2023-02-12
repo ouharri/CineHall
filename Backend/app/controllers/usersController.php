edbebef2db0f79160db803b9a64cec2a64d1d948
ouharri <otmanouhrri@gmail.com>
2023-02-12 05:37:46 +0000

    update v12.77

--- Backend/app/controllers/usersController.php
+++ Backend/app/controllers/usersController.php
@@ -347,7 +347,7 @@ class usersController
                                 $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
                                 $mail->SMTPAuth = true;                                   //Enable SMTP authentication
                                 $mail->Username = 'atman.atharri@gmail.com';                     //SMTP username
-                                $mail->Password = 'xakztnzayygnxzkc';                               //SMTP password
+                                $mail->Password = 'ljodmasbogdyckhk';                               //SMTP password
                                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
                                 $mail->Port = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
                                 //Recipients
